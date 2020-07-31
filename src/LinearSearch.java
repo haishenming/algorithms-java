07c45a44c66146b005a29aa10ab486d8ed69866f
xiang.yufan <xiang.yufan@tslsmart.com>
2020-07-31 16:03:20 +0800

    从简单的线性查找开始

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,7 +1,8 @@
-
 // 从简单的线性查找开始
 public class LinearSearch {
-    public int search(int[] data, int target) {
+    private LinearSearch(){}
+
+    public static int search(int[] data, int target) {
         for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
                 return i;
@@ -14,10 +15,10 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-        LinearSearch ls = new LinearSearch();
-
-        int res = ls.search(data, 3);
-
+        int res = LinearSearch.search(data, 3);
         System.out.println(res);
+
+        int res2 = LinearSearch.search(data, 10);
+        System.out.println(res2);
     }
 }
